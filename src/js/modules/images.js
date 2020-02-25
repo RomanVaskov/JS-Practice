7bb22c83a5a08b5c680177cd9e2caf0402c72aa5
Roman <romavaskov@yandex.ru>
2020-02-25 10:10:39 +0300

    Проект полностью готов, внесены последние правки

--- src/js/modules/images.js
+++ src/js/modules/images.js
@@ -1,7 +1,8 @@
 const images = () => {
     const imgPopup = document.createElement('div'),
         workSection = document.querySelector('.works'),
-        bigImg = document.createElement('img');
+        bigImg = document.createElement('img'),
+        scroll = caclScroll();
 
     imgPopup.classList.add('popup');
     workSection.appendChild(imgPopup);
@@ -20,6 +21,7 @@ const images = () => {
         if (target && target.classList.contains('preview')) {
             imgPopup.style.display = 'flex';
             document.body.style.overflow = 'hidden';
+            document.body.style.marginRight = `${scroll}px`;
 
             const path = target.parentNode.getAttribute('href');
             bigImg.setAttribute('src', path);
@@ -28,8 +30,24 @@ const images = () => {
         if (target && target.matches('div.popup')) {
             imgPopup.style.display = 'none';
             document.body.style.overflow = '';
+            document.body.style.marginRight = `0px`;
         }
     });
+
+    function caclScroll() {
+        let div = document.createElement('div');
+
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
+    }
 };
 
 export default images;
